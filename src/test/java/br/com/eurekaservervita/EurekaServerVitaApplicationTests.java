5560e352ad8c51dc371f8b57fd6c59e6617d0eba
douglaspio <douglasp.alves@hotmail.com>
2022-07-28 23:05:39 -0300

    Exportação para o repositório

---
+++ src/test/java/br/com/eurekaservervita/EurekaServerVitaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eurekaservervita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerVitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
